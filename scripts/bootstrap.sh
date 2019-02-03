8ad30c8e942902a43b5a0a9616f90b850a3bce13
Matt Gaunt <mattgaunt@google.com>
2019-02-02 17:52:22 -0800

    Trying a different approach

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -39,7 +39,13 @@ function installNoSudo() {
   echo -e "GREP STATUS --------------> ${grepStatus}"
   if ! [[ $grepStatus -eq 0 ]]; then
     echo -e "RUNNING --------------------"
-    curl -sL "https://raw.githubusercontent.com/glenpike/npm-g_nosudo/master/npm-g-nosudo.sh" | bash -
+    curl -sL "https://raw.githubusercontent.com/glenpike/npm-g_nosudo/master/npm-g-nosudo.sh" | sh -
+#    bash <(curl -s "https://raw.githubusercontent.com/glenpike/npm-g_nosudo/master/npm-g-nosudo.sh?$(date +%s)") &> ${ERROR_LOG}
+#    printf '%s' '
+#export NPM_PACKAGES="/home/pi/.npm-packages"
+#export NODE_PATH="$NPM_PACKAGES/lib/node_modules${NODE_PATH:+:$NODE_PATH}"
+#export PATH="$NPM_PACKAGES/bin:$PATH"
+#' >> "${HOME}/.bashrc"
   fi
   echo -e "\n\t✅  Done\n"
 }
