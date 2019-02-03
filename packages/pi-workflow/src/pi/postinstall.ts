2c390953a3032b01a08662f406e6947ee37aa012
Matt Gaunt <mattgaunt@google.com>
2019-02-02 19:03:32 -0800

    Adding some logging to help diagnose any issues

--- packages/pi-workflow/src/pi/postinstall.ts
+++ packages/pi-workflow/src/pi/postinstall.ts
@@ -8,16 +8,19 @@ export async function postInstall() {
   const pkgPath = path.join(__dirname, '..', '..', 'package.json')
   const pkg = await fs.readJSON(pkgPath);
 
-  // Get the RCLocal file
-  const rcLocal = await getRCLocal();
-  const commands = rcLocal.getCommands();
   const commandsToRun = [
     `echo "@hopin/pi-workflow ${pkg.version} installed."`,
     `npm install -g @hopin/pi-workflow`,
     // TODO: Add a script that starts @hopin/pi-workflow to listen for requests
   ];
+
+  const rcLocal = await getRCLocal();
+  const commands = rcLocal.getCommands();
+  logger.log(`rc.local currently has ${commands.length} command(s)`);
+
   for (const c of commandsToRun) {
     if (commands.indexOf(c) == -1) {
+      logger.log(`Adding '${c}' to rc.local...`);
       await rcLocal.addCommand(c);
     }
   }
